b1ac04395208c1db2e48de786741194936a9da32
Nick <n.drobyshevski@pm.me>
2026-01-13 17:45:40 +0100

    Structure initiale

---
+++ src/main/java/com/wad/player/PlayerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wad.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerServiceApplication.class, args);
+    }
+}
